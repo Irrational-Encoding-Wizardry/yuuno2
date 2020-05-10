274e5f39c06e5fd46bce06858bb23dbd6066d82a
StuxCrystal <stuxcrystal@encode.moe>
2020-05-10 15:24:22 +0000

    Add Symlink to javascript

---
+++ python/yuuno2-notebook/yuuno2notebook/__init__.py
@@ -0,0 +1,7 @@
+def _jupyter_nbextension_paths():
+    return [{
+        'section': 'notebook',
+        'src': 'javascript/notebook',
+        'dest': 'yuuno2notebook',
+        'require': 'yuuno2notebook'
+    }]
