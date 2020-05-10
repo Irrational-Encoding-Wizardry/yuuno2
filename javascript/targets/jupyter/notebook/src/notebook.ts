1837b7638d171dc4426571393c62cca2c6a8c5e9
StuxCrystal <stuxcrystal@encode.moe>
2020-05-10 15:32:02 +0000

    Starting with the actual notebook extension

---
+++ javascript/targets/jupyter/notebook/src/notebook.ts
@@ -0,0 +1,5 @@
+import { RemoteClip } from '@yuuno2/remote';
+
+export function load_ipython_extension() {
+    console.log("Loading Yuuno2 for Jupyter Notebook");
+}
---
+++ javascript/targets/jupyter/notebook/webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+
+
+const babel = (...additional) => [{
+    loader: "babel-loader",
+    options: {
+        presets: ["@babel/preset-env"]
+    },
+    ...additional
+}]
+const style = (...additional) => [
+    'style-loader', {
+        loader: 'css-loader',
+        options: {
+            sourceMap: true
+        }
+    },
+    ...additional
+]
+
+module.exports = {
+    entry: './src/notebook.ts',
+    module: {
+        rules: [
+            {
+                test: /\.m?jsx?/,
+                use: babel(),
+                exclude: /node_modules/
+            },
+            {
+                test: /.\tsx?$/,
+                use: babel(),
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: style(),
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    output: {
+        filename: 'yuuno2notebook.js',
+        path: path.resolve(__dirname, "lib"),
+        
+        libraryTarget: "amd",
+        auxiliaryComment: "Yuuno2 Jupyter Notebook (JupyterLab)"
+    },
+    devtool: "source-map"
+};
